dbd0221308621ff3ad58d2bf684cc5ce928341e4
unknown <malanchuksam@gmail.com>
2020-02-12 21:58:17 -0500

    completed tasks 08-10

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,14 +10,14 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
+y = [1, 2, 3, 4, 5]
 
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [num**3 for num in (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)]
 
 print(y)
 
@@ -26,7 +26,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [food.capitalize() for food in a]
 
 print(y)
 
@@ -36,6 +36,7 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+# make the input variables a integer before doing math
+y = [int(num) for num in x if int(num) % 2 == 0]
 
 print(y)
---
+++ src/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+def is_even(num):
+#     return true if num % 2 == 0
